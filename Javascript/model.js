6a271b1ea1fb908025835a8864a409893cdd7b73
Tomas Nøklegaard <88710155+TomasFrame117@users.noreply.github.com>
2022-03-21 12:28:51 +0100

    update

---
+++ Javascript/model.js
@@ -0,0 +1,56 @@
+const model = {
+
+    view: '',
+    currentpage: 'store',
+    totalitems: 0,
+    totalprice: 0,
+    shopingcart: [],
+
+    products:[
+        {
+            name: ' Simple swedder',
+            price: 349,
+            instock: 10,
+            info: 'Fin genser, laget i merino Ull',
+            gender: 'him' || 'her',
+            image: "/bilder/produkter/swedder_unisex_FRONT.png",
+            
+        },
+        {
+            name: 'Classic Milanien Hoddie',
+            price: 349,
+            instock: 10,
+            info: 'Fin genser, laget i merino Ull',
+            gender: 'him' || 'her',
+            image: "/bilder/produkter/classic_swedder_unisex_FRONT.png",
+    
+            
+        },
+        {
+            name: 'Cropped Swedder Gray',
+            price: 399,
+            instock: 1,
+            info: 'Cozy swedder for warmer days, to spread the Milanien way.',
+            gender: 'her', 
+            image: "/bilder/produkter/simple_cropped_Hoodie_MEL_back_FRONT.png",
+        },
+        {
+            name: 'Swedder Ukriane Edition',
+            price: 349,
+            instock: 5,
+            info: 'Fin genser, laget i merino Ull',
+            gender: 'her' || 'him',
+            image: "./bilder/produkter/classic_swedder_unisex_FRONT.png",
+            
+        },
+        {
+            name: 'Milanien Cap',
+            price: 149,
+            instock: 20,
+            info: 'Olabukse',
+            him: 'him', 
+            image: "/bilder/produkter/.png",
+        },
+
+    ]
+}
