e58e0c5f12abe90581cfe2a6900655179dbf8ec2
Tomas Nøklegaard <88710155+TomasFrame117@users.noreply.github.com>
2022-03-21 10:09:35 +0100

    comit

---
+++ Javascript/controlller.js
@@ -0,0 +1,44 @@
+function addToCart(i){
+    if(model.products[i].instock == 0) alert('not in stock');
+    else{
+        model.products[i].instock --;
+        model.totalitems ++;
+        model.shopingcart.push({
+            name: model.products[i].name,
+            price: model.products[i].price,
+            image: model.products[i].image,
+            })
+    }
+    viewCartPage();
+}
+
+// function changeimg(){
+//     var i = 0;
+//     var images = [];
+//     var time = 3000;
+
+//     images[0] = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKOD6BR-r0HnTZPMX_2LSzzmj23_1W2WrRrA&usqp=CAU",
+//     images[1] = "/bilder/produkter/classic_swedder_unisex_FRONT.png",
+
+//     document.slide.src=images[i];
+
+//     if(i<images.length - 1) {
+//         i++;
+//     } else {
+//         i = 0;
+//     }
+//     setTimeout("changeimg()",time)
+//     viewHomePage();
+// }
+// function cangehomepicture() {
+//      const image = [
+//          'url("/bilder/IMG_270.jpg" onclick ="viewShop")',
+//          'url("/20210307_150214.jpg")',
+//      ]
+
+//      const take = document.getElementsByClassName('slideShowBox');
+//      const cahngeImg = image[Math.floor(Math.random() * image.length)];
+//     take.style.slideShowPic = cahngeImg;
+//      take.style.backgroundSize = "contain";
+//  }
+//  setInterval(cangehomepicture, 2000)
